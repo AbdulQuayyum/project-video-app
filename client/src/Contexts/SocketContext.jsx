3c930a7d5e67762189ce91659e2fa8bd5e0feeec
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-07-19 23:42:44 +0100

    Added the logic for the for the answering calls, leaving calls and calling another in the context file...

---
+++ client/src/Contexts/SocketContext.jsx
@@ -0,0 +1,89 @@
+import React, { createContext, useState, useRef, useEffect } from 'react';
+import { io } from 'socket.io-client';
+import Peer from 'simple-peer';
+
+const SocketContext = createContext();
+const socket = io(`${import.meta.env.VITE_SERVER_URL}`)
+
+const SocketProvider = ({ children }) => {
+    const [callAccepted, setCallAccepted] = useState(false);
+    const [callEnded, setCallEnded] = useState(false);
+    const [stream, setStream] = useState();
+    const [name, setName] = useState('');
+    const [call, setCall] = useState({});
+    const [me, setMe] = useState('');
+
+    const MyVideo = useRef();
+    const UserVideo = useRef();
+    const ConnectionRef = useRef();
+
+    useEffect(() => {
+        navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+            .then((currentStream) => {
+                setStream(currentStream);
+
+                MyVideo.current.srcObject = currentStream;
+            });
+
+        socket.on('me', (id) => setMe(id));
+
+        socket.on('callUser', ({ from, name: callerName, signal }) => {
+            setCall({ isReceivingCall: true, from, name: callerName, signal });
+        });
+    }, [])
+
+    const AnswerCall = () => {
+        setCallAccepted(true);
+
+        const peer = new Peer({ initiator: false, trickle: false, stream });
+
+        peer.on('signal', (data) => {
+            socket.emit('answerCall', { signal: data, to: call.from });
+        });
+
+        peer.on('stream', (currentStream) => {
+            UserVideo.current.srcObject = currentStream;
+        });
+
+        peer.signal(call.signal);
+
+        ConnectionRef.current = peer;
+    };
+
+    const CallUser = () => {
+        const peer = new Peer({ initiator: true, trickle: false, stream });
+
+        peer.on('signal', (data) => {
+            socket.emit('callUser', { userToCall: id, signalData: data, from: me, name });
+        });
+
+        peer.on('stream', (currentStream) => {
+            UserVideo.current.srcObject = currentStream;
+        });
+
+        socket.on('callAccepted', (signal) => {
+            setCallAccepted(true);
+
+            peer.signal(signal);
+        });
+
+        ConnectionRef.current = peer;
+    }
+
+    const LeaveCall = () => {
+        setCallEnded(true);
+
+        ConnectionRef.current.destroy();
+
+        window.location.reload();
+    };
+
+    return (
+        <SocketContext.Provider
+            value={{ call, callAccepted, MyVideo, UserVideo, stream, name, setName, callEnded, me, CallUser, LeaveCall, AnswerCall }}>
+            {children}
+        </SocketContext.Provider>
+    )
+}
+
+export { SocketProvider, SocketContext }
--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { Background } from "./Theme/Index"
+import { SocketProvider } from './Contexts/SocketContext'
 import { ThemeProvider } from './Contexts/ThemeContext'
 import App from './App.jsx'
 import "./Styles/Index.css"
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Background>
       <ThemeProvider>
-        <App />
+        <SocketProvider>
+          <App />
+        </SocketProvider>
       </ThemeProvider>
     </Background>
   </React.StrictMode>,

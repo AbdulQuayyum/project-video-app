7577804ba5c65db58e8f532cf8a2030da2ce16c0
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-07-19 19:51:09 +0100

    Added Server Side Logic...

--- Server/Server.js
+++ Server/Server.js
@@ -1,6 +1,35 @@
 const express = require('express')
 const app = express()
+const server = require("http").createServer(app);
+const cors = require("cors");
+
 const port = 8080
 
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+app.use(cors());
+
 app.get('/', (req, res) => res.send('Heyy There!'))
-app.listen(port, () => console.log(`Project is running on port ${port}!`))
+
+io.on("connection", (socket) => {
+    socket.emit("me", socket.id);
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callEnded")
+    });
+
+    socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+        io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+    });
+
+    socket.on("answerCall", (data) => {
+        io.to(data.to).emit("callAccepted", data.signal)
+    });
+});
+
+server.listen(port, () => console.log(`Project is running!`))

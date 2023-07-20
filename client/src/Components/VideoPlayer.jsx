a5e32f308ef163daade0da3cf907ec6dbb5784cf
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-07-20 12:49:31 +0100

    Added the video component...

--- client/src/Components/VideoPlayer.jsx
+++ client/src/Components/VideoPlayer.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react';
+
+import { SocketContext } from '../Contexts/SocketContext';
 
 const VideoPlayer = () => {
+    const { name, callAccepted, MyVideo, UserVideo, callEnded, stream, call } = useContext(SocketContext);
+
     return (
-        <div>VideoPlayer</div>
+        <div className='flex flex-col gap-8 md:flex-row'>
+            {stream && (
+                <div className='flex flex-col items-center'>
+                    <span className='font-extrabold  text-[#aaa] pb-5'>{name || "Name"}</span>
+                    <video playsInline muted ref={MyVideo} autoPlay className="w-full max-w-md rounded-3xl" />
+                </div>
+            )}
+            {callAccepted && !callEnded && (
+                <div className='flex flex-col items-center'>
+                    <span className='font-extrabold  text-[#aaa] pb-5'>{call.name || "Name"}</span>
+                    <video playsInline ref={UserVideo} autoPlay className="w-full max-w-md rounded-3xl" />
+                </div>
+            )}
+        </div>
     )
 }
 
